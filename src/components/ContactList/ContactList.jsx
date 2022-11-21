2e4be98681e3f089c92d8ab5b749472ed765bf56
VolodymyrTashak <vovavozny33@gmail.com>
2022-11-21 23:12:08 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { deleteContact } from 'redux/contactsSlice';
-import { getContact, getFilter } from 'redux/selectors';
+import { getContact } from 'redux/selectors';
 
 import { ContactItem, ContactBtn, ContactInfo } from './ContactList.styled';
 
 export const ContactList = () => {
   const contacts = useSelector(getContact);
-  const filter = useSelector(getFilter);
+  // const filter = useSelector(getFilter);
   const dispatch = useDispatch();
 
   const filteredContact = () => {
