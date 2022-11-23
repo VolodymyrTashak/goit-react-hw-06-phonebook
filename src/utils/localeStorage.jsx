613e40fa4d596a067e73781cf48a358adf06f9f6
VolodymyrTashak <vovavozny33@gmail.com>
2022-11-23 21:48:26 +0200

    finish

--- src/utils/localeStorage.jsx
+++
@@ -1,7 +0,0 @@
-export const localStorageGetItem = item => {
-  JSON.parse(localStorage.getItem(item));
-};
-
-export const localStorageSetItem = (item, object) => {
-  localStorage.setItem(item, JSON.stringify(object));
-};
